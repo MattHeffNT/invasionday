572e8ea57e1483153c639969b7bd1c61e2e87c3a
MattHeffNT <heffa2004@gmail.com>
2023-01-22 00:11:41 +1100

    font awesome and safer selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.scss';
-
 // css and html design by https://codepen.io/brundolf/pen/beagbQ
 
 function App() {
@@ -13,9 +12,11 @@ function App() {
   const toggleSelection = (e) => {
     const yes = document.querySelector('#yes');
     const no = document.querySelector('#no');
-    const upDown = e.nativeEvent.srcElement.offsetParent.className;
 
-    if (upDown == 'down') {
+    const upDown = e.currentTarget.className;
+
+    // if up or down pressed toggle css classes on/off
+    if (upDown === 'down') {
       yes.classList.remove('selected');
       no.classList.add('selected');
     } else if (upDown == 'up') {
@@ -26,28 +27,24 @@ function App() {
 
   // when you press the A button
   const aButton = () => {
-
-    //UseState so that i can return different things to the gameboy screen
-
     const selected = document.querySelector('.selected').id;
     const screen = document.querySelector('.display');
-    const screenText = document.querySelector('.displayAnswers');
 
     if (selected == 'yes') {
-      // display gameover -- play derisive sound
-      console.log('game over');
+      // display gameover -- show bbq area video for education
       setShow(true);
-      document.querySelector('.display>p').innerHTML = `<p> Game over ya dumb dog </p>`;
+      document.querySelector('.display>p').innerHTML = `<p > Game over ya dumb dog </p>`;
+
+      document.querySelector('.display>p').style = 'font-size:8px;';
     } else {
       // you win display fireworks...play music
       screen.classList.add('displayGif');
       screen.innerHTML = `<p> Congratulations, you're not gammin!!! </p>`;
       audio.play();
-
-      // setShow(false);
     }
   };
 
+  // if start button pressed reset
   const restart = () => {
     window.location.reload(false);
   };
@@ -65,21 +62,25 @@ function App() {
           POWER
         </div>
 
+        {/* Main game screen */}
         <div className="display" id="mainCanvas">
           <p> Do you Celebrate Invasion Day ?</p>
 
+          {/* show different things on screen depending on what option selected  */}
           <div className="displayAnswers">
             {show ? (
-              (<h1>Game Over</h1>),
-              <iframe
-                width="200"
-                height="115"
-                src="https://www.youtube.com/embed/NqcFg4z6EYY"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
-              ></iframe>
+              ((<h1>Game Over</h1>),
+              (
+                <iframe
+                  width="200"
+                  height="120"
+                  src="https://www.youtube.com/embed/NqcFg4z6EYY"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen
+                ></iframe>
+              ))
             ) : (
               <div>
                 <p
@@ -92,17 +93,23 @@ function App() {
                   - Yes
                 </p>
                 <p id="no" name="option" value="No">
-                  - No I'm not a dumb dog
+                  - No, I'm not a dumb dog
                 </p>
               </div>
             )}
           </div>
-          <p style={{ paddingTop: '3em' }}> press start to restart </p>
+          {show ? <div /> : <p style={{ paddingTop: '3em' }}> press start to restart </p>}
         </div>
 
         <div className="label">
           <div className="title">GAME BOY</div>
-          <div className="subtitle"></div>
+          <div className="subtitle">
+            <span className="c">C</span>
+            <span className="o1">O</span>
+            <span className="l">L</span>
+            <span className="o2">O</span>
+            <span className="r">R</span>
+          </div>
         </div>
       </div>
 
@@ -113,15 +120,19 @@ function App() {
           <div className="up" onClick={(e) => toggleSelection(e)}>
             <i className="fa fa-caret-up"></i>
           </div>
+
           <div className="right">
             <i className="fa fa-caret-right"></i>
           </div>
-          <div className="down">
-            <i className="fa fa-caret-down" onClick={(e) => toggleSelection(e)}></i>
+
+          <div className="down" onClick={(e) => toggleSelection(e)}>
+            <i className="fa fa-caret-down"></i>
           </div>
+
           <div className="left">
             <i className="fa fa-caret-left"></i>
           </div>
+
           <div className="middle"></div>
         </div>
         <div className="a-b">
