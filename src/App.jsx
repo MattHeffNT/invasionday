a862d0b172a8517469f75a30f550a37bab196c96
MattHeffNT <heffa2004@gmail.com>
2024-01-18 09:05:48 +1100

    updated src for video

--- src/App.jsx
+++ src/App.jsx
@@ -67,17 +67,17 @@ function App() {
           <div className="displayAnswers">
             {show ? (
               ((<h1>Game Over</h1>),
-              (
-                <iframe
-                  width="200"
-                  height="120"
-                  src="https://www.youtube.com/embed/NqcFg4z6EYY"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowfullscreen
-                ></iframe>
-              ))
+                (
+                  <iframe
+                    width="200"
+                    height="120"
+                    src="https://www.youtube.com/embed/gYtz4hH2z7A?si=bPV95VxzSfeP62Xe"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
+                ))
             ) : (
               <div>
                 <p
